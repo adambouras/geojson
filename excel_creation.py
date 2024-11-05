30160ea79ecc516db2d83666daf3d8d384788d40
adam bouras <adambouras1@gmail.com>
2024-11-04 22:09:01 -0500

    add new files and edits

--- excel_creation.py
+++ excel_creation.py
@@ -1,11 +1,10 @@
 import pandas as pd
-# read csv files:
 import glob
 import os
 from collections import defaultdict
 os.chdir('/Users/adambouras/Downloads/')
 # Get a list of all CSV files in the directory
-csv_files = glob.glob('11_01_2024_edp_seed_file_2024_11_*.csv')
+csv_files = glob.glob('*.csv')
 print(csv_files)
 # Create an empty list to store the dataframes
 dict = {}
@@ -18,26 +17,21 @@ try:
     for i, file in enumerate(csv_files):
         # if i <3:
             df = pd.read_csv(file, dtype=str)
-            df['cdm_code'] = "'"+df['cdm_code']
-            df['cdmx_code']= "'"+df['cdmx_code']
-        
-            if 'cdm_field' in df.columns.tolist():
-                cdm_field = df['cdm_field'].unique()[0]
-                cdm_table = df['cdm_table'].unique()[0]
-                if cdm_table in dict.keys():
-                    dict[f"{cdm_table}"].update({f"{cdm_field}":df})
-                else:
-                    dict.update({f"{cdm_table}":{f"{cdm_field}":df.astype(str)}})
+            childkey = df['childkey'].unique()[0]
+            parentkey = df['parentkey'].unique()[0]
+            if childkey in dict.keys():
+                dict[f"{parentkey}"].update({f"{childkey}":df})
+            else:
+                dict.update({f"{parentkey}":{f"{childkey}":df.astype(str)}})
                      
 
 except Exception as e:
     print(f'Error reading file {file}: {str(e)}')
 # print(dict['cancer_related_surgical_procedure'])
-     
 os.chdir('/Users/adambouras/excel_merge/')
-for cdm_table in dict:
-    print(dict[cdm_table].keys())
-    with pd.ExcelWriter(f'{cdm_table}.xlsx') as writer:
-        for cdm_field in dict[cdm_table].keys():
-            print(dict[cdm_table][cdm_field])
-            dict[cdm_table][cdm_field].to_excel(writer, sheet_name=cdm_field, index=False, na_rep='NULL')
+for parentkey in dict:
+    print(dict[parentkey].keys())
+    with pd.ExcelWriter(f'{parentkey}.xlsx') as writer:
+        for childkey in dict[parentkey].keys():
+            print(dict[parentkey][childkey])
+            dict[parentkey][childkey].to_excel(writer, sheet_name=childkey, index=False, na_rep='NULL')
